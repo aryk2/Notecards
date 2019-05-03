6c09f7f08eec7dc942f65c8e75254d690315622b
aryk2 <aryk2@pdx.edu>
2019-05-03 10:36:10 -0700

    just starting to work on some of the file directory access stuff, more to come soon

--- notecards.cpp
+++ notecards.cpp
@@ -1,13 +1,60 @@
 #include "notecards.h"
 
-int test_readin();
 
 int main() {
-    test_readin();
+    int choice = 1;
+    while(choice) {
+        cout << "\nNotecard Program options:\n"
+             << "1. add a new set\n"
+             << "2. view existing sets\n"
+             << "0 - EXIT\n"
+             << "choice: ";
+    
+        cin >> choice; 
+        if(choice == 1)
+            add_set();
+        if(choice == 2)
+            view_notefolder();
+    }
     return 0;
 }
 
-int test_readin() {
+int add_set() {
+
+}
+
+int view_notefolder() {
+    node * head;
+    DIR * Dir;
+    struct dirent * DirEntry;
+    Dir = opendir("notefolder");
+     
+    cout << "\nOptions: \n";
+    while(DirEntry = readdir(Dir)) {
+        if(DirEntry->d_name[0] != '.') {
+	    cout << DirEntry->d_name << endl;
+            add_to_lll_end(head, DirEntry->d_name);
+        }
+    }
+    
+    cout << "\nwhich set would you like to open:\n";
+}
+
+void add_to_lll_end(node * & head, char * entry) {
+    node * temp = head;
+    node * new_node = new node;
+    new_node -> term = new char [strlen(entry) +1];
+    strcpy(new_node -> term, entry);
+    while(temp -> next) 
+        temp = temp -> next;
+    temp -> next = new_node; 
+}
+
+void clear_lll(node * & head) {
+
+} 
+//example
+/*
     ifstream myfile;
     string line;
     myfile.open ("notefolder/notes.txt");
@@ -16,5 +63,4 @@ int test_readin() {
             cout << line << '\n';
         myfile.close();
     }
-    return 0;
-}
+    */
--- notecards.h
+++ notecards.h
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <cstring>
+#include <cctype>
 #include <fstream>
 #include <string>
+#include <dirent.h>
 using namespace std;
 
 class node {
@@ -11,3 +14,25 @@ class node {
         char * definition;
         node * next;
 };
+
+node::node() {
+    term = NULL;
+    definition = NULL;
+    next = NULL;
+}
+
+node::~node() {
+    if(term) {
+        delete [] term;
+        term = NULL;
+    }
+    if(definition) {
+        delete [] definition;
+        definition = NULL;
+    }
+    next = NULL;
+}
+//prototypes
+int add_set();
+int view_notefolder();
+void add_to_lll_end(node * & head, char * entry);
